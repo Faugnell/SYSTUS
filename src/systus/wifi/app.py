be8305fceded7dd5dbd0caf5ba0169be6c569295
AmAntoZ <118252365+AmAntoZ@users.noreply.github.com>
2026-06-02 09:24:57 +0200

    wifi base

--- src/systus/wifi/app.py
+++ src/systus/wifi/app.py
@@ -4,14 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>SYSTUS</h1>
-    <p>Serveur Flask opérationnel</p>
-    """
+    return render_template("index.html")
+
+@app.route("/wifi", methods=["POST"])
+def wifi():
+    ssid = request.form["ssid"]
+    password = request.form["password"]
+
+    print("SSID:", ssid)
+    print("PASSWORD:", password)
+
+    return f"Connexion en cours à {ssid}"
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=5000, debug=True)
